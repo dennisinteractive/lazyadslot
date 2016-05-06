85dd96d7127c7aaeac91bdffc78641b6a2d93de5
Cristina Llamas <tucarapareceunarodilla@gmail.com>
2016-05-06 14:10:11 +0100

    submodule renamed

--- lazyadslot_wysiwyg/plugins/wysiwyg/lazyadslot/lazyadslot.js
+++ lazyadslot_wysiwyg/plugins/wysiwyg/lazyadslot/lazyadslot.js
@@ -1,16 +1,16 @@
 /**
  * @file
- * Dennis DFP Inline WYSIWYG plugin
+ * Lazyadslot Inline WYSIWYG plugin
  */
 (function() {
 
-  Drupal.wysiwyg.plugins.dfpinline = {
+  Drupal.wysiwyg.plugins.lazyadslotWysiwyg = {
 
     /**
      * Execute the button.
      */
     invoke: function (data, settings, instanceId) {
-      var content = (data.format === 'html') ? this._getPlaceholder(settings) : settings.dfpInlinePattern;
+      var content = (data.format === 'html') ? this._getPlaceholder(settings) : settings.lazyadslotWysiwygPattern;
 
       Drupal.wysiwyg.instances[instanceId].insert(content);
     },
@@ -19,11 +19,11 @@
      * Replace all placeholders with images.
      */
     attach: function (content, settings) {
-      var dfpInlinePattern = settings.dfpInlinePattern;
+      var lazyadslotWysiwygPattern = settings.lazyadslotWysiwygPattern;
       var placeholder = this._getPlaceholder(settings);
       // Some WYSIWYGs (CKEditor) will strip the slash from single tags:
       // <foo /> becomes <foo>
-      var inlineAdSlot = dfpInlinePattern.replace(/\/>/, '/?>').replace(/ /g, ' ?');
+      var inlineAdSlot = lazyadslotWysiwygPattern.replace(/\/>/, '/?>').replace(/ /g, ' ?');
 
       // Remove unnecessary paragraph.
       var pattern = new RegExp('<p>' + inlineAdSlot + '</p>', 'ig');
@@ -42,30 +42,30 @@
     },
 
     /**
-     * Replace images with <!--#dfpinline#--> tags in content upon detaching editor.
+     * Replace images with lazyadslotWysiwygPattern tags in content upon detaching editor.
      */
     detach: function(content, settings) {
-      var dfpInlinePattern = settings.dfpInlinePattern;
+      var lazyadslotWysiwygPattern = settings.lazyadslotWysiwygPattern;
       // Some WYSIWYGs (CKEditor) will strip the slash from single tags:
       // <foo /> becomes <foo>
-      var inlineAdSlot = dfpInlinePattern.replace(/\/>/, '/?>').replace(/ /g, ' ?');
+      var inlineAdSlot = lazyadslotWysiwygPattern.replace(/\/>/, '/?>').replace(/ /g, ' ?');
       // Replace (duplicate) placeholders within p tags with a single break.
       // var newContent = content.replace(/\s*<p[^>]*?>(?:\s*<img(?:\s*\w+=['"][^'"]*?['"]\s*)*?\s*class=['"][^'"]*?dfp-inline-placeholder[^'"]*?['"]\s*(?:\s*\w+=['"][^'"]*?['"]\s*)*?(?:\/)?>\s*)+<\/p>\s*/ig, dfpInlinePattern);
 
       // Replace all other placeholders.
-      var newContent = content.replace(/<img(?:\s*\w+=['"][^'"]*?['"]\s*)*?\s*class=['"][^'"]*?dfp-inline-placeholder[^'"]*?['"]\s*(?:\s*\w+=['"][^'"]*?['"]\s*)*?(?:\/)?>/ig, dfpInlinePattern);
+      var newContent = content.replace(/<img(?:\s*\w+=['"][^'"]*?['"]\s*)*?\s*class=['"][^'"]*?inline-body-placeholder[^'"]*?['"]\s*(?:\s*\w+=['"][^'"]*?['"]\s*)*?(?:\/)?>/ig, lazyadslotWysiwygPattern);
       // Fix paragraphs opening just before breaks.
       var pattern = new RegExp('(?:' + inlineAdSlot + ')*(<p[^>]*?>\\s*)' + inlineAdSlot, 'ig');
-      newContent = newContent.replace(pattern, dfpInlinePattern + '$1');
+      newContent = newContent.replace(pattern, lazyadslotWysiwygPattern + '$1');
       // Remove duplicate breaks and any preceding whitespaces.
       pattern = new RegExp('(?:\\s*' + inlineAdSlot + '){2,}' + inlineAdSlot, 'ig');
-      newContent = newContent.replace(pattern, dfpInlinePattern);
+      newContent = newContent.replace(pattern, lazyadslotWysiwygPattern);
       // Fix paragraphs ending after breaks.
       pattern = new RegExp(inlineAdSlot + '(\\s*<\/p>)(?:' + inlineAdSlot + ')*', 'ig');
-      newContent = newContent.replace(pattern, '$1' + dfpInlinePattern);
+      newContent = newContent.replace(pattern, '$1' + lazyadslotWysiwygPattern);
       // Remove duplicate breaks with trailing whitespaces.
       pattern = new RegExp('(?:' + inlineAdSlot + '\\s*){2,}', 'ig');
-      newContent = newContent.replace(pattern, dfpInlinePattern);
+      newContent = newContent.replace(pattern, lazyadslotWysiwygPattern);
 
       return newContent;
     },
@@ -74,7 +74,7 @@
      * Helper function to return a HTML placeholder.
      */
     _getPlaceholder: function (settings) {
-      return '<img src="' + settings.path + '/images/placeholder.png" alt="&lt;--#dfpinline#--&gt;" title="&lt;--#dfpinline#--&gt;" class="dfp-inline-placeholder drupal-content" />';
+      return '<img src="' + settings.path + '/images/placeholder.png" alt="&lt;--#lazyadslotWysiwyg#--&gt;" title="&lt;--#lazyadslotWysiwyg#--&gt;" class="inline-body-placeholder drupal-content" />';
     }
   };
 
